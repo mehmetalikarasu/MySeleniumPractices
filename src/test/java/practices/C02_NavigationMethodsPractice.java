21bb9d3aa286ce15ceaf68f9790a758f29da477d
mehmet ali karasu <mak.0416csgo@gmail.com>
2023-05-31 16:47:59 +0300

    Selenium practice navigation()

---
+++ src/test/java/practices/C02_NavigationMethodsPractice.java
@@ -0,0 +1,34 @@
+package practices;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_NavigationMethodsPractice {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //Sayfayi Max buyuklukte yapmak icin...
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); //sayfanin tum elementlerinin yuklemesini beklemesi icin...
+
+            //1. Youtube ana sayfasina gidelim . https://www.youtube.com/
+        driver.get("https://www.youtube.com/");
+
+            //2. Amazon soyfasina gidelim. https://www.amazon.com/
+        driver.get("https://www.amazon.com/");
+
+            //3. Tekrar YouTube’sayfasina donelim
+        driver.navigate().back();
+
+            //4. Yeniden Amazon sayfasina gidelim
+        driver.navigate().forward();
+
+            //5. Sayfayi Refresh(yenile) yapalim
+        driver.navigate().refresh();
+
+            //6.Sayfayi kapatalim / Tum sayfalari kapatalim
+        driver.quit(); // "quit" tum sekmeleri kapatir,"close" ise sadece driverin ustunde oldugu sekmeyi...
+
+    }
+}
